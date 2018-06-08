b9751f1113e7e199fa3b54f8cd66275499ad8f87
RinkySaha <39879999+RinkySaha@users.noreply.github.com>
2018-06-08 14:53:08 +0600

    Form validation with Post (basic)
    Form validation with Post (basic)

---
+++ formpost1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Validation</title> 
+</head>
+<body>
+	<form action="store.php" method="POST">
+		<input type="text" name="fname">
+		<input type="text" name="lname">
+		<input type="submit" name="save" value="save">
+	</form>
+
+</body>
+</html>
---
+++ formpost1/store.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "I am stored <br>";
+
+print_r($_POST);
+
+echo "<br>";
+
+$num1 = $_POST['fname'];
+echo  $num1;
