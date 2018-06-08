77b4e372eb20df1c80b9575f9a17a49f7a8d41f1
RinkySaha <39879999+RinkySaha@users.noreply.github.com>
2018-06-08 14:52:38 +0600

    Form validation with get (basic)
    Form validation with get (basic)

---
+++ formget1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Validation</title>
+</head>
+<body>
+	<form action="store.php" method="GET">
+		<input type="text" name="fname">
+		<input type="text" name="lname">
+		<input type="submit" name="save" value="save">
+	</form>
+
+</body>
+</html>
---
+++ formget1/store.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "I am stored <br>";
+
+print_r($_GET);
